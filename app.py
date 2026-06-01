0451b24c0ba689ae4304a8dca80f90f61f9a5ff1
R1que1 <mjosecarlos12@gmail.com>
2026-05-31 22:41:59 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1754,7 +1754,8 @@ if __name__ == "__main__":
 
     port = int(os.environ.get("PORT", 10000))
 
-    app.run(
+    socketio.run(
+        app,
         host="0.0.0.0",
         port=port,
         debug=False
